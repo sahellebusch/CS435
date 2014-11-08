9ab07ec7f1384e08635166f0550a96b9842e288a
sahellebusch <sahellebusch@gmail.com>
2014-11-08 16:57:55 -0600

    working project

--- monte_carlo/monte_pi.c
+++ monte_carlo/monte_pi.c
@@ -1,10 +1,9 @@
 /* Author         =>  Sean Hellebusch
  * Revision       =>  1
- * Date           =>  2014-10-09
+ * Date           =>  2014-11-08
  *
- * This program determines common meetings times among
- * three people using concurrency.  The times are
- * predefined in a text file.
+ * This program simulates the Monte Carlo
+ * method to find the value of pi.
  */
 
 #include <pthread.h>
@@ -15,8 +14,8 @@
 
 #define MILLION 1000000
 
-int num_sims, darts_thrown, n_sim, hits = 0;
-int done = 1;
+int num_sims, darts_thrown, n_sim, hits;
+int done      = -1;
 int can_print = -1;
 pthread_mutex_t *random_lock;
 pthread_mutex_t *sim_source_lock;
@@ -25,119 +24,106 @@ pthread_cond_t  *print_ready;
 
 void *simulator(void *argc)
 {
-    printf("created simulator\n");
-    while (!done)
-    {
-        pthread_mutex_lock(random_lock);
-        printf("i've got the random lock");
-        double x = (double)rand() / (double)RAND_MAX;
-        double y = (double)rand() / (double)RAND_MAX;
-        printf("I'm giving up the random lock");
-        pthread_mutex_unlock(random_lock);
+  while (done < 0)
+  {
+    pthread_mutex_lock(random_lock);
+    double x = (double)rand() / (double)RAND_MAX;
+    double y = (double)rand() / (double)RAND_MAX;
+    pthread_mutex_unlock(random_lock);
 
-        pthread_mutex_lock(sim_source_lock);
-        darts_thrown++;
-        printf("darts thrown = %i\n", darts_thrown );
-        if (darts_thrown == num_sims)
-        {
-            done = -1;
-        }
-        if (darts_thrown % MILLION == 0)
-        {
-            can_print = -1;
-        }
-        if (sqrt(x * x + y * y) <= 1)
-        {
-            hits++;
-        }
-        pthread_mutex_unlock(sim_source_lock);
-        if (can_print)
-        {
-            printf("can print, throws = %i", num_sims);
-            pthread_cond_broadcast(print_ready);
-        }
-    }
-    pthread_exit(NULL);
+    pthread_mutex_lock(sim_source_lock);
+    darts_thrown++;
+    if(darts_thrown == num_sims)
+      done = 0;
+    if((darts_thrown > 0) && (darts_thrown % MILLION == 0))
+      can_print = 0;
+    if(sqrt(x*x + y*y) <= 1)
+      hits++;
+    pthread_mutex_unlock(sim_source_lock);
+    if(can_print == 0)
+      pthread_cond_broadcast(print_ready);
+  }
+  pthread_exit(NULL);
 }
 
 void *printer(void *argc)
 {
-    printf("created printer\n");
-    double curr_area = 0;
-    while (!done)
-    {
+  double curr_estimate;
+  while(done < 0)
+  {
+    pthread_mutex_lock(print_lock);
+    // while we cannot print, wait and go to sleep
+    while(can_print < 0 )
+      pthread_cond_wait(print_ready, print_lock);
 
-        pthread_mutex_lock(print_lock);
-        printf("print = %i.\n", can_print);
-        while (!can_print)
-        {
-            printf("here\n");
-            pthread_cond_wait(print_ready, print_lock);
-            printf("here\n");
-        }
-
-        curr_area = hits / num_sims;
-        printf("Simulation number: %i.  Current calculated area: %f", num_sims, curr_area);
-    }
-    pthread_exit(NULL);
+    curr_estimate = (((double)hits) / darts_thrown) * 4;
+    printf("darts thrown = %i, pi: %f\n", darts_thrown, curr_estimate);
+    can_print = -1;
+    pthread_mutex_unlock(print_lock);
+  }
+  pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[])
 {
-    int num_sim_threads, i = 0;
-    srand((unsigned) time(NULL));
-    if (argc != 3)
+  int num_sim_threads, i = 0;
+  srand((unsigned) time(NULL));
+  if(argc != 3)
+  {
+    printf( "Error: Incorrect number of arguments.\n" );
+    exit(-1);
+  }
+
+  sscanf (argv[1], "%d", &num_sim_threads);
+  sscanf (argv[2], "%i", &num_sims);
+
+  random_lock = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
+  pthread_mutex_init(random_lock, NULL);
+  sim_source_lock = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
+  pthread_mutex_init(sim_source_lock, NULL);
+  print_lock = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
+
+  print_ready = (pthread_cond_t *) malloc (sizeof(pthread_cond_t));
+  pthread_cond_init(print_ready, NULL);
+
+  // Create printer(consumer) threads
+  pthread_t *printer_thread = (pthread_t *) malloc (sizeof(pthread_t));
+
+  if(pthread_create (printer_thread, NULL, printer, NULL))
+  {
+    fprintf(stderr, "Error creating simulator thread %i.\n", i);
+    exit(-1);
+  }
+
+  // Create simulation(producer) threads
+  pthread_t **simulation_thread = (pthread_t **) malloc (sizeof(pthread_t *) * num_sim_threads);
+  for(i = 0; i < num_sim_threads; i++)
+  {
+    simulation_thread[i] = (pthread_t *) malloc (sizeof(pthread_t));
+
+    if(pthread_create (simulation_thread[i], NULL, simulator, NULL))
     {
-        printf( "Error: Incorrect number of arguments.\n" );
-        exit(-1);
+      fprintf(stderr, "Error creating simulator thread %i.\n", i);
+      exit(-1);
     }
+  }
 
-    sscanf (argv[1], "%d", &num_sim_threads);
-    sscanf (argv[2], "%i", &num_sims);
-
-    printf("num_sim_threads = %i, num_sims = %i\n", num_sim_threads, num_sims);
-
-    random_lock = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
-    pthread_mutex_init(random_lock, NULL);
-    sim_source_lock = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
-    pthread_mutex_init(sim_source_lock, NULL);
-    print_lock = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
-
-    // Create printer(consumer) threads
-    pthread_t *printer_thread = (pthread_t *) malloc (sizeof(pthread_t));
-
-    if (pthread_create (printer_thread, NULL, printer, NULL))
+  // join simulation threads
+  for(i = 0; i < num_sim_threads; i++)
+  {
+    if(pthread_join(*simulation_thread[i], NULL))
     {
-        fprintf(stderr, "Error creating simulator thread %i.\n", i);
-        exit(-1);
+      fprintf(stderr, "Error joining simulator thread %i.\n", i);
+      exit(-1);
     }
+  }
 
-    // Create simulation(producer) threads
-    pthread_t **simulation_thread = (pthread_t **) malloc (sizeof(pthread_t *) * num_sim_threads);
-    for (i = 0; i < num_sim_threads; i++)
-    {
-        simulation_thread[i] = (pthread_t *) malloc (sizeof(pthread_t));
+  // join printer thread
+  if(pthread_join(*printer_thread, NULL))
+  {
+    fprintf(stderr, "Error joining simulator thread %i.\n", i);
+    exit(-1);
+  }
 
-        if (pthread_create (simulation_thread[i], NULL, simulator, NULL))
-        {
-            fprintf(stderr, "Error creating simulator thread %i.\n", i);
-            exit(-1);
-        }
-    }
-
-    for ( i = 0; i < num_sim_threads; i++)
-    {
-        if (pthread_join(*simulation_thread[i], NULL))
-        {
-            fprintf(stderr, "Error joining simulator thread %i.\n", i);
-            exit(-1);
-        }
-    }
-
-    if (pthread_join(*printer_thread, NULL))
-    {
-        fprintf(stderr, "Error joining simulator thread %i.\n", i);
-        exit(-1);
-    }
-    exit(0);
+  exit(0);
 }
