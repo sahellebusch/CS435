4d0dd9aba0e5af9dbc7ca5e756ace1b762a31451
sahellebusch <sahellebusch@gmail.com>
2014-11-11 14:51:03 -0600

    cleaned up program. added documentation

--- monte_carlo/monte_pi.c
+++ monte_carlo/monte_pi.c
@@ -10,49 +10,57 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
 #include <math.h>
 
 #define MILLION 1000000
 
+int done      = 0;
+int can_print = 0;
 int num_sims, darts_thrown, n_sim, hits;
-int done      = -1;
-int can_print = -1;
+// mutex to protect the random number generator (not thread safe)
 pthread_mutex_t *random_lock;
-pthread_mutex_t *sim_source_lock;
-pthread_mutex_t *print_lock;
+// mutex to protect shared variables
+pthread_mutex_t *var_lock;
 pthread_cond_t  *print_ready;
 
 /*
  * Function: simulator
  *
- * Simulates throwing a dart as per the Monte Carlo 
+ * Simulates throwing a dart as per the Monte Carlo
  * method of finding area in a region.  Alerts the
  * printer when it can print.
  *
  */
 void *simulator()
 {
-  while (done < 0)
+  while (!done)
   {
     pthread_mutex_lock(random_lock);
+
     double x = (double)rand() / (double)RAND_MAX;
     double y = (double)rand() / (double)RAND_MAX;
+
     pthread_mutex_unlock(random_lock);
 
-    pthread_mutex_lock(sim_source_lock);
-    
+    pthread_mutex_lock(var_lock);
+
     darts_thrown++;
     if(darts_thrown == num_sims)
-      done = 0;
-    if((darts_thrown > 0) && (darts_thrown % MILLION == 0))
-      can_print = 0;
-    if(sqrt(x*x + y*y) <= 1)
+      done = ~done;
+
+    if(sqrt(x * x + y * y) <= 1)
       hits++;
-    if(can_print == 0)
-      pthread_cond_broadcast(print_ready);
-    pthread_mutex_unlock(sim_source_lock);
-    
+
+    // if we have a multiple of a million, change condition var to
+    // satisfy printer's predicate and signal.
+    if((darts_thrown > 0) && (darts_thrown % MILLION == 0))
+    {
+      can_print = ~can_print;
+      pthread_cond_signal(print_ready);
+    }
+
+    pthread_mutex_unlock(var_lock);
+
   }
 
   pthread_exit(NULL);
@@ -69,23 +77,21 @@ void *printer()
 {
   double curr_estimate;
 
-  while(done < 0)
+  while(!done)
   {
-    // lock the printer so it cannot be interrupted
-    pthread_mutex_lock(print_lock);
-    
+
+    pthread_mutex_lock(var_lock);
+
     // while we cannot print, wait and go to sleep
-    while(can_print < 0 )
-      pthread_cond_wait(print_ready, print_lock);
+    while(!can_print)
+      pthread_cond_wait(print_ready, var_lock);
 
     // calculate current estimate of pi
     curr_estimate = (((double)hits) / darts_thrown) * 4;
     printf("Pi: %f\n", curr_estimate);
-    
-    can_print = -1;
+    can_print = ~can_print;
 
-    // unlock the printer
-    pthread_mutex_unlock(print_lock);
+    pthread_mutex_unlock(var_lock);
   }
 
   pthread_exit(NULL);
@@ -105,18 +111,27 @@ int main(int argc, char *argv[])
   sscanf (argv[1], "%d", &num_sim_threads);
   sscanf (argv[2], "%i", &num_sims);
 
+  /*
+   * Our program is required to print the current estimate of pi every
+   * 1 million simulations.  Don't waste CPU resources if the user enters
+   * less than a million simulations.  Just exit.
+   */
+  if(num_sims < MILLION)
+  {
+    printf("You have entered less than one million simulations, no estimate will be displayed.\n");
+    exit(-1);
+  }
   // malloc the locks
   random_lock = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
   pthread_mutex_init(random_lock, NULL);
-  sim_source_lock = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
-  pthread_mutex_init(sim_source_lock, NULL);
-  print_lock = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
+  var_lock    = (pthread_mutex_t *) malloc (sizeof (pthread_mutex_t));
+  pthread_mutex_init(var_lock, NULL);
 
-  // malloc the pthread_cond variable
+  // malloc the condition variable
   print_ready = (pthread_cond_t *) malloc (sizeof(pthread_cond_t));
   pthread_cond_init(print_ready, NULL);
 
-  // Create printer(consumer) threads
+  // Create printer(consumer) thread
   pthread_t *printer_thread = (pthread_t *) malloc (sizeof(pthread_t));
 
   if(pthread_create (printer_thread, NULL, printer, NULL))
@@ -155,5 +170,9 @@ int main(int argc, char *argv[])
     exit(-1);
   }
 
+  //clean up and exit cleanly
+  pthread_cond_destroy(print_ready);
+  pthread_mutex_destroy(var_lock);
+  pthread_mutex_destroy(random_lock);
   exit(0);
 }
