cc3b3d5bedbe33880380d05a2d91c26915fa6d55
sahellebusch <sahellebusch@gmail.com>
2014-12-02 13:14:09 -0600

    Added program skeleton.

--- JThreadsCMT/src/HellebuschCMT.java
+++ JThreadsCMT/src/HellebuschCMT.java
@@ -1,9 +1,55 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class HellebuschCMT {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws FileNotFoundException {
+		final int NUM_THREADS = 5;
+		final int NUM_ITERATIONS = 10;
+		
+		System.out.println("Main start");
+		
+		//create arrays to hold frames ad threads
+		Searcher[] searcher = new Searcher[NUM_THREADS];
+		Thread[] thread = new Thread[NUM_THREADS];
+		
+		// create objects that will run as threads
+		for(int i = 0; i < NUM_THREADS; i++) {
+			searcher[i] = new Searcher();
+		}
+		
+		//set searcher names and iterations
+		for(int i = 0; i < NUM_THREADS; i++) {
+			searcher[i].setName("T" + i);
+			searcher[i].setNumIterations(NUM_ITERATIONS);
+		}
+		
+		//create the threads
+		for(int i = 0; i < NUM_THREADS; i++) {
+			thread[i] = new Thread(searcher[i]);
+		}
+		
+		for(int i = 0; i < NUM_THREADS; i++) {
+			System.out.println("Starting thread " + searcher[i].getName());
+			thread[i].start();
+			
+			System.out.println("Thread " + searcher[i].getName() + " started.");
+		}
+		
+		for(int i = 0; i < NUM_THREADS; i++) {
+			try {
+				thread[i].join();
+				System.out.println("Joining thread " + searcher[i].getName());
+			} catch(InterruptedException e) {
+				System.out.println("Thread " + searcher[i].getName() + "failed to join.");
+			}
+		}
+		
+		
+		System.out.println("Main exiting.");
 	}
 
 }
--- JThreadsCMT/src/Searcher.java
+++ JThreadsCMT/src/Searcher.java
@@ -1,4 +1,42 @@
+public class Searcher implements Runnable {
+	private int numIterations;
+	private String name;
+	
+	public Searcher(String name) {
+		this.name = name;		
+		System.out.println("Searcher constructor called.");
+	}
+	
+	public Searcher() {
+		name = null;
+		numIterations = 1;
+		System.out.println("Nameless thread created.");
+	}
+	
+	public void run() {
+		System.out.println(name + " run() has been called.");
+		for(int i = 0; i < numIterations; i++) {
+			System.out.println(name + "is on iteration " + i);
+		}
+		
+		System.out.println(name + " is exiting.");
+	}
 
-public class Searcher {
+	public int getNumIterations() {
+		return numIterations;
+	}
 
+	public void setNumIterations(int numIterations) {
+		this.numIterations = numIterations;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
